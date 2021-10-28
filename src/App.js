2782130fdd38d6c79761af8b9fe4af76355bdaf3
Adam Ross Benton <adamrossbenton@gmail.com>
2021-10-27 20:59:51 -0700

    codesandbox SUUUUUUUUUUCKS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import "./styles.css"
+import weatherData from "./weatherData"
+import WeatherForecast from "./components/WeatherForecast"
 
 function App() {
+
+  const allWeather = weatherData.map((ele,index) => (
+    <WeatherForecast
+      img={ele.img}
+      conditions={ele.conditions}
+      time={ele.time}
+    />
+  ))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>My WeatherIcons App</h1>
+      <section>
+        {allWeather}
+      </section>
     </div>
   );
 }
